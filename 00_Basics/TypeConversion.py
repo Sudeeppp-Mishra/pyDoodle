09c6430dbe4944aa4eadf1c85b0b6fd86fe11497
mishrasm2022@gmail.com <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-06-16 16:33:05 +0545

    Type conversion of int, float and complex added

---
+++ 00_Basics/TypeConversion.py
@@ -0,0 +1,19 @@
+x = 1 # int 
+y = 34.9 # float
+z = 4j # complex
+
+# convert from int to float:
+a = float(x)
+
+# convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print("Type:", type(a), "Value:", a)
+
+print("Type:", type(b), "Value:", b)
+
+print("Type:", type(c), "Value:", c)
+
