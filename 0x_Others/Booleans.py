dcd8d8debb3bcb6e2d26575fbf3e4be28437a8c4
mishrasm2022@gmail.com <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-06-16 17:43:30 +0545

    Booleans added

---
+++ 0x_Others/Booleans.py
@@ -0,0 +1,27 @@
+# Boolean expression answers either True or False
+
+print(10>8)
+print(10==8)
+print(10<8)
+
+# Evaluate any alue by using bool() function, it gives you True or False in return
+
+print("\n")
+print(bool("Hello"))
+print(bool(14))
+
+######
+# Most values are true
+
+print(bool("")) # it returns False as it is empty string otherwise every other stirng is True
+
+### Values that are false
+# bool(False)
+# bool(None)
+# bool(0)
+# bool("")
+# bool(())
+# bool([])
+# bool({})
+
+# Functions can return boolean
