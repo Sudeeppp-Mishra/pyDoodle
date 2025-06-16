fdfb314345f969a23bb8891246324653dfaf05b6
mishrasm2022@gmail.com <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-06-16 15:10:16 +0545

    first.py reupdated to previous version

--- 00_Basics/first.py
+++ 00_Basics/first.py
@@ -1,6 +1,3 @@
 # Your First Python Program
 print("Hello World!")
 
-# NOTE: comments in python starts '#', and Python will render the rest of the line as comment
-# Purpose of comment for in-code documentation
-# This is a comment
