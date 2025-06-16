6974dccafc18bc57b3d032b1ed27b5184c3f8174
mishrasm2022@gmail.com <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-06-16 15:03:07 +0545

    Indentation.py file added

---
+++ 00_Basics/Indentation.py
@@ -0,0 +1,26 @@
+# Python Indentation
+# Indentation refers to the spaces at the beginning of a code line.
+
+# Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+
+# Python uses indentation to indicate a block of code.
+
+#Example:
+if 5>2:
+    print(5,"is greater than",2);
+   
+    
+# Python will give you an error if you skip the indentation:
+
+# Syntax Error:    
+# if 5 > 2:
+# print("Five is greater than two!")
+
+
+
+# The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
