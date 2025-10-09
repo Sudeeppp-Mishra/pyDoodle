893b19fb30a44d7370b7330fb4fc2644b0be7531
mishrasm2022@gmail.com <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-10-09 16:58:15 +0545

    feat: Tkinter(GUI) window concepts added

---
+++ 14_GUI(Tkinter)/1_window.py
@@ -0,0 +1,14 @@
+from tkinter import * # imports Tk GUI's library
+
+# Widgets = GUI elements: buttons, textboxes, labels, images
+# Windows = serves as a container to hold or contain these widgets
+
+window = Tk() # instantiate an instance of a window
+
+window.geometry("420x420") # width x height
+window.title("WOWWW😭")
+
+window.config(background="#94cbf4")
+
+window.mainloop() # place window on computer screen, listen for events
+
