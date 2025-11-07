03fbaebad9ef113a468232c50783e4db1f51487f
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-07 14:40:49 +0545

    feat: added comments for better understanding

--- 14_GUI(PySide)/6_QMainWindow/mainwindow.py
+++ 14_GUI(PySide)/6_QMainWindow/mainwindow.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtGui import QAction
+from PySide6.QtGui import QAction, QKeySequence
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -9,8 +9,18 @@ class MainWindow(QMainWindow):
         # Menubar and menus
         menu_bar = self.menuBar()
         file_menu = menu_bar.addMenu("&File")
+        # Notes on Menu Bar and '&' in PySide6 / PyQt
+
+        # - In PySide6/PyQt, menu bars are created using `QMenuBar` and menu items with `QMenu` and `QAction`.
+        # - The `&` symbol in menu or action names is used to **define a keyboard shortcut (mnemonic)**.
         
-        new_action = QAction("╳", self)
-        file_menu.addAction(new_action)
-        new_action.triggered.connect(self.close)
+        # Example:
+
+        # ```python
+        # file_menu = menu_bar.addMenu("&File")  # 'F' becomes the mnemonic; Alt + F to open it; but in mac it's not visible visually
+        # new_action = QAction("&New", self)     # 'N' becomes the mnemonic
+        
+        quit_action = QAction("╳", self)
+        file_menu.addAction(quit_action)
+        quit_action.triggered.connect(self.close)
         
