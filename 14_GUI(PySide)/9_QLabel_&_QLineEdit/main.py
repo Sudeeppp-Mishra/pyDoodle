3da6f4b2bf373bda1b147344f67eb36449099a59
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-12 18:44:22 +0545

    feat: add button and text holder label

--- 14_GUI(PySide)/9_QLabel_&_QLineEdit/main.py
+++ 14_GUI(PySide)/9_QLabel_&_QLineEdit/main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout
 
 class Widget(QWidget):
     def __init__(self):
@@ -9,6 +9,8 @@ class Widget(QWidget):
         # A set of signal we can connect to 
         label = QLabel("Fullname: ")
         self.line_edit = QLineEdit()
+        button = QPushButton("Grab Data")
+        self.text_holder_label = QLabel("I AM HERE")
         
 app = QApplication(sys.argv)
 widget = Widget()
