8e365705298ffaf6842094befccea3a032b1dbe1
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-12 18:27:33 +0545

    feat: add window title

--- 14_GUI(PySide)/9_QLabel_&_QLineEdit/main.py
+++ 14_GUI(PySide)/9_QLabel_&_QLineEdit/main.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLay
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("QLabel and QLineEdit")
         
 app = QApplication(sys.argv)
 widget = Widget()
