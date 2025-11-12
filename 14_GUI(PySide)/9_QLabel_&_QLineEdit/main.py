4dffb84c08435f1f17628aa5843c495da9dcbf2f
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-12 18:20:41 +0545

    feat: add a new class called Widget that inherits from QWidget and inside the constructor of it call the super constructor

--- 14_GUI(PySide)/9_QLabel_&_QLineEdit/main.py
+++ 14_GUI(PySide)/9_QLabel_&_QLineEdit/main.py
@@ -1,2 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
