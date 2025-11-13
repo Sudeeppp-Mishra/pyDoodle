89153e70ffe597d8af88db9451cd8a6a656cd1a7
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-13 18:24:06 +0545

    feat: connect copy, cut, undo and redo button to predefined slots

--- 14_GUI(PySide)/10_QTextEdit/main.py
+++ 14_GUI(PySide)/10_QTextEdit/main.py
@@ -10,14 +10,19 @@ class Widget(QWidget):
         
         # Buttons
         copy_button = QPushButton("Copy")
+        copy_button.clicked.connect(self.text_edit.copy)
         
         cut_button = QPushButton("Cut")
+        cut_button.clicked.connect(self.text_edit.cut)
         
         paste_button = QPushButton("Paste")
+        copy_button.clicked.connect(self.text_edit.paste)
         
         undo_button = QPushButton("Undo")
+        undo_button.clicked.connect(self.text_edit.undo)
         
         redo_button = QPushButton("Redo")
+        redo_button.clicked.connect(self.text_edit.redo)
         
         set_plain_text_button = QPushButton("Set Plain Text")
         
