b07923bf82ac091c1478736bfec5c75654b27e26
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-18 15:24:16 +0545

    feat: add push buttons and add them to hbox layout then add all hbox layouts to vbox layout

--- 14_GUI(PySide)/12_SizePoliciesAndStretches/main.py
+++ 14_GUI(PySide)/12_SizePoliciesAndStretches/main.py
@@ -15,6 +15,23 @@ class Widget(QWidget):
         h_layout_1.addWidget(label)
         h_layout_1.addWidget(line_edit)
         
+        button_1 = QPushButton("One")
+        button_2 = QPushButton("Two")
+        button_3 = QPushButton("Three")
+        
+        # stretch: how much of the available space (in the layout) is occupied by each widget.
+        # you specify the stretch when you add things to the layout : button_1 takes up 2 units, button_2 and button_3 take up 1 unit
+        
+        h_layout_2 = QHBoxLayout()
+        h_layout_2.addWidget(button_1, 2)
+        h_layout_2.addWidget(button_2, 1)
+        h_layout_2.addWidget(button_3, 1)
+        
+        v_layout = QVBoxLayout()
+        v_layout.addLayout(h_layout_1)
+        v_layout.addLayout(h_layout_2)
+        
+        self.setLayout(v_layout)
         
 app = QApplication(sys.argv)
 widget = Widget()
