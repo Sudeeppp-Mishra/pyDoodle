45bd09572edcda4c5a6b423569be7844b88b1da9
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-18 15:20:35 +0545

    feat: add label and line edit to a HBoxLayout

--- 14_GUI(PySide)/12_SizePoliciesAndStretches/main.py
+++ 14_GUI(PySide)/12_SizePoliciesAndStretches/main.py
@@ -1,11 +1,19 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QSizePolicy
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QSizePolicy, QLineEdit
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
         
         self.setWindowTitle("Size Policies and Stretches")
+        
+        # Size Policy: how the widgets behaves if container space is expanded or shrunk
+        label = QLabel("Some Text: ")
+        line_edit = QLineEdit()
+        
+        h_layout_1 = QHBoxLayout()
+        h_layout_1.addWidget(label)
+        h_layout_1.addWidget(line_edit)
         
         
 app = QApplication(sys.argv)
