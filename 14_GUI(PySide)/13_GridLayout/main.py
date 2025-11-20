2fb5b51e1d4386e2e26d56809d5992959a32ee98
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-20 06:16:01 +0545

    feat: add basic code snippet

---
+++ 14_GUI(PySide)/13_GridLayout/main.py
@@ -0,0 +1,12 @@
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+app = QApplication(sys.argv)
+widget = Widget()
+
+widget.show()
+sys.exit(app.exec())
