f599c4dd47448e84e5b9180492539b0b49e44505
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-20 06:20:16 +0545

    feat: add bunch of buttons to see demo

--- 14_GUI(PySide)/13_GridLayout/main.py
+++ 14_GUI(PySide)/13_GridLayout/main.py
@@ -6,6 +6,14 @@ class Widget(QWidget):
         super().__init__()
         self.setWindowTitle("QGridLayout Demo")
         
+        button_1 = QPushButton("One")
+        button_2 = QPushButton("Two")
+        button_3 = QPushButton("Three")
+        button_4 = QPushButton("Four")
+        button_5 = QPushButton("Five")
+        button_6 = QPushButton("Six")
+        button_7 = QPushButton("Seven")
+        
 app = QApplication(sys.argv)
 widget = Widget()
 
