0f19844185d141edd3c77785f7d4890f17219a72
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-20 06:19:14 +0545

    feat: add window title

--- 14_GUI(PySide)/13_GridLayout/main.py
+++ 14_GUI(PySide)/13_GridLayout/main.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("QGridLayout Demo")
         
 app = QApplication(sys.argv)
 widget = Widget()
