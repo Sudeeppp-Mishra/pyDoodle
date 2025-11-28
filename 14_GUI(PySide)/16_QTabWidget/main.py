a74d1b648535b9d2a7ec83f371f276266d18b595
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-29 05:40:55 +0545

    feat: add window title

--- 14_GUI(PySide)/16_QTabWidget/main.py
+++ 14_GUI(PySide)/16_QTabWidget/main.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QApplication, QWidget
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("QTabWidget Demo")
 
 app = QApplication(sys.argv)
 widget = Widget()
