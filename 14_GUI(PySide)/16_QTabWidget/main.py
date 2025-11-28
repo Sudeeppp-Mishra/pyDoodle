35998556e18b66f1306a927039c1a3bb7a23e359
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-29 05:44:33 +0545

    feat: add information tab

--- 14_GUI(PySide)/16_QTabWidget/main.py
+++ 14_GUI(PySide)/16_QTabWidget/main.py
@@ -1,10 +1,21 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QTabWidget, QLineEdit, QLabel, QHBoxLayout
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("QTabWidget Demo")
+        
+        tab_widget = QTabWidget(self)
+        
+        # Information
+        widget_form = QWidget()
+        label_full_name = QLabel("Full name: ")
+        line_edit_full_name = QLineEdit()
+        form_layout = QHBoxLayout()
+        form_layout.addWidget(label_full_name)
+        form_layout.addWidget(line_edit_full_name)
+        widget_form.setLayout(form_layout)
 
 app = QApplication(sys.argv)
 widget = Widget()
