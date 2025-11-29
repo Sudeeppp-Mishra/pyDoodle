85f91cc26d008263c0644a211af31f2ac6bd331d
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-29 05:48:05 +0545

    feat: add button's tab

--- 14_GUI(PySide)/16_QTabWidget/main.py
+++ 14_GUI(PySide)/16_QTabWidget/main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget, QTabWidget, QLineEdit, QLabel, QHBoxLayout
+from PySide6.QtWidgets import QApplication, QWidget, QTabWidget, QLineEdit, QLabel, QHBoxLayout, QPushButton, QVBoxLayout
 
 class Widget(QWidget):
     def __init__(self):
@@ -16,6 +16,21 @@ class Widget(QWidget):
         form_layout.addWidget(label_full_name)
         form_layout.addWidget(line_edit_full_name)
         widget_form.setLayout(form_layout)
+        
+        # Buttons
+        widget_buttons = QWidget()
+        button_1 = QPushButton("One")
+        button_1.clicked.connect(self.button_1_clicked)
+        button_2 = QPushButton("Two")
+        button_3 = QPushButton("Three")
+        buttons_layout = QVBoxLayout()
+        buttons_layout.addWidget(button_1)
+        buttons_layout.addWidget(button_2)
+        buttons_layout.addWidget(button_3)
+        widget_buttons.setLayout(buttons_layout)
+        
+    def button_1_clicked(self):
+        pass
 
 app = QApplication(sys.argv)
 widget = Widget()
