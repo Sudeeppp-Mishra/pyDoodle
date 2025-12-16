29a9c6c7e5784bf53d072dd7a513348ab89d13a9
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-12-16 15:55:39 +0545

    feat: init Combo box and add items to it

--- 14_GUI(PySide)/17_QComboBox/main.py
+++ 14_GUI(PySide)/17_QComboBox/main.py
@@ -1,10 +1,18 @@
-from PySide6.QtWidgets import QWidget, QApplication
+from PySide6.QtWidgets import QWidget, QApplication, QComboBox
 import sys
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("QComboBox Demo")
+        
+        self.combo_box = QComboBox(self);
+        # Add planets to the combobox
+        self.combo_box.addItem("Earth")
+        self.combo_box.addItem("Venus")
+        self.combo_box.addItem("Mars")
+        self.combo_box.addItem("Jupiter")
+        self.combo_box.addItem("Saturn")
         
 app = QApplication(sys.argv)
 widget = Widget()
